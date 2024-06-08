723515e4f6f6019bc5f8d2a7429834ee46d09a85
nonrita <2220025@ecc.ac.jp>
2024-06-08 20:03:31 +0900

    feature: axiosでmain.jsonの値を取得しました

--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const header = ref('aa');
+
+const getTeams = ()=>{
+  axios.get('src/data/main.json').then((res) =>{
+    header.value = res.data;
+  })  
+}
+
+onMounted(async()=>{
+  await getTeams();
+})
+</script>
+
+
 <template>
     <h1>top</h1>
+    <div v-for="(component, i) in header" :key="i">
+        {{ component }}
+    </div>
 </template>
